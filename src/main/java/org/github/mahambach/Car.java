77bfe745faac60d8eb042696584919592e7a50e5
Lueder Budde <lueder.budde@gmail.com>
2024-01-31 10:56:53 +0100

    create Car class

---
+++ src/main/java/org/github/mahambach/Car.java
@@ -0,0 +1,6 @@
+package org.github.mahambach;
+// Step 2: Create a derived class "Car" that inherits from "Vehicle" and has additional properties like "Number of Doors".
+public class Car extends Vehicle{
+    private int numberOfDoors;
+    private String color;
+}
