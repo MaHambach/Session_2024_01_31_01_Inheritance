3fc2a62a7d5bc6251582ed9431d17d6b3c9fe645
jessi <jessi.nowski@hotmail.de>
2024-01-31 11:01:05 +0100

    add toString in Vehicle

--- src/main/java/org/github/mahambach/Vehicle.java
+++ src/main/java/org/github/mahambach/Vehicle.java
@@ -5,4 +5,13 @@ public class Vehicle {
     private String manufacturer;
     private String model;
     private int yearOfManufacture;
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "manufacturer='" + manufacturer + '\'' +
+                ", model='" + model + '\'' +
+                ", yearOfManufacture=" + yearOfManufacture +
+                '}';
+    }
 }
