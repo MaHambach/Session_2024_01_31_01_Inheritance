fc83ec27d281a6b32a1909af07d47ba47b0de0a7
mahambach <ma.hambach@gmx.net>
2024-01-31 10:53:33 +0100

    add Class Vehicle

--- src/main/java/org/github/mahambach/Main.java
+++ src/main/java/org/github/mahambach/Main.java
@@ -1,5 +1,15 @@
 package org.github.mahambach;
 
+/*Create a new Java project and implement the following steps to practice inheritance and classes in Java.
+
+Step 1: Create a base class "Vehicle" with properties like "Manufacturer", "Model", and "Year of Manufacture".
+Step 2: Create a derived class "Car" that inherits from "Vehicle" and has additional properties like "Number of Doors".
+Step 3: Implement a method in the "Vehicle" class that outputs information about the vehicle.
+Step 4: Override the method from step 3 in the "Car" class to also output the number of doors.
+Step 5: Create instances of the "Vehicle" and "Car" classes and call the methods to output the information.
+Please post the link to your GitHub repo here, where you have solved this task together. inputfield
+If you have already completed this task earlier, you may work on the bonus task on the next page.
+*/
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ src/main/java/org/github/mahambach/Vehicle.java
@@ -0,0 +1,8 @@
+package org.github.mahambach;
+
+public class Vehicle {
+
+    private String manufacturer;
+    private String model;
+    private int yearOfManufacture;
+}
